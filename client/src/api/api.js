53dc47d6fa304fffbe219d8f0f8c3e323d86dbbb
Jitesh Singh <singhjitesh469@gmail.com>
2026-02-02 15:26:59 +0530

    Add vercel rewrite for SPA routing

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
+
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api",
+  // baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api",
+  baseURL: API_BASE,
   // baseURL: import.meta.env.VITE_API_URL || "/api",
 });
 
