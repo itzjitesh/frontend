81087af074d29d46ddd8a6ac7567ace9d8575b51
Jitesh Singh <singhjitesh469@gmail.com>
2026-02-10 15:32:09 +0530

    Updated Header Hero banner section

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import TextType from "../Sections/text";
+import Carousel from "../Sections/carousel_cards";
+
+function Banner() {
+  return (
+    <section className="banner-sec bg-img  h-[50%] w-auto bg-[url(./assets/banner-assets/bnr.jpg)] bg-red-500 ">
+      <div className="page-center h-full">
+        <div className="bnr-wrap flex items-center justify-between p-[90px 0px] h-full">
+          <div className="bnr-left w-[48%]">
+            <h1 className="text-white">
+              <TextType
+                text={[
+                  "Text typing effect",
+                  "for your websites",
+                  "Happy coding!",
+                ]}
+                typingSpeed={75}
+                pauseDuration={1500}
+                showCursor
+                cursorCharacter="_"
+                texts={[
+                  "Welcome to React Bits! Good to see you!",
+                  "Build some amazing experiences!",
+                ]}
+                deletingSpeed={50}
+                variableSpeedEnabled={false}
+                variableSpeedMin={60}
+                variableSpeedMax={120}
+                cursorBlinkDuration={0.5}
+              />
+            </h1>
+          </div>
+          <div className="bnr-right w-[48%] flex items-center justify-center">
+            {/* user cards here */}
+            <div className="h-[340px] relative">
+              <Carousel
+                baseWidth={330}
+                autoplay
+                autoplayDelay={3000}
+                pauseOnHover
+                loop
+                round={false}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Banner;
