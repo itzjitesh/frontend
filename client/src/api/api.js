97e0111754f029d348c028d1e1a04359fa8ee41f
Jitesh Singh <singhjitesh469@gmail.com>
2026-02-10 12:47:48 +0530

    api_base updated

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API_BASE =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api";
+  import.meta.env.VITE_API_BASE_URL ||
+  "https://valuelens-z0dm.onrender.com/api";
 
 const API = axios.create({
   // baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api",
